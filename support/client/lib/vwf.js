b248dcdb9df639083ed0de495d1dcfef19d335d0
Eric Burns <eric.burns@lmco.com>
2013-09-24 16:03:21 -0400

    Have kernel try calling createdProperty, initializedProperty, satProperty in turn
    refs #2589

--- support/client/lib/vwf.js
+++ support/client/lib/vwf.js
@@ -4529,7 +4529,6 @@ if ( ! childComponent.source ) {
 
             var node = nodes.existing[ nodeID ];
             var modelFunc;
-            var viewFunc;
             var propertyHasBeenSet;
             var stopAfterSet;
 
@@ -4557,20 +4556,17 @@ if ( ! childComponent.source ) {
                 case "create":
                     node.properties.create( propertyName );
                     modelFunc = "creatingProperty";
-                    viewFunc = "createdProperty";
                     thisEntry.creating = true;
                     stopAfterSet = false;
                     break;
                 case "initialize":
                     node.properties.create( propertyName );
                     modelFunc = "initializingProperty";
-                    viewFunc = "initializedProperty";
                     thisEntry.initializing = true;
                     stopAfterSet = false;
                     break;
                 case "set":
                     modelFunc = "settingProperty";
-                    viewFunc = "satProperty";
                     stopAfterSet = true;
                     break;
                 default:
@@ -4664,9 +4660,23 @@ if ( ! childComponent.source ) {
             // The entry that assigns the value calls the viewFunc on each view
             if ( assigned ) {
                 that.views.forEach( function( view ) {
-                    view[ viewFunc ] &&
-                    view[ viewFunc ]( nodeID, propertyName, propertyValue,
-                                      propertyGet, propertySet );
+                    switch ( action ) {
+                        case "create":
+                            if ( view.createdProperty ) {
+                                view.createdProperty( nodeID, propertyName, propertyValue,
+                                              propertyGet, propertySet );
+                                break;
+                            }
+                        case "initialize":
+                            if ( view.initializedProperty ) {
+                                view.initializedProperty( nodeID, propertyName, propertyValue );
+                                break;
+                            }
+                        case "set":
+                            view.satProperty && 
+                            view.satProperty( nodeID, propertyName, propertyValue );
+                            break;
+                    }
                 } );
             }
 
